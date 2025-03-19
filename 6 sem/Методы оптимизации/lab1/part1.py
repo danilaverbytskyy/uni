879a7cef6b5aaeeade573ec1bd715a97ede33e84
Danila <dod.shreder@gmail.com>
2025-03-19 13:24:39 +0300

    МО 1.1 1.2

--- 6 sem/Методы оптимизации/lab1/part1.py
+++ 6 sem/Методы оптимизации/lab1/part1.py
@@ -1,13 +1,19 @@
+N = 0 # кол-во вычислений функций
+k = -1 # кол-во итераций
+
 def f(arg):
+    global N
+    N = 2*(k + 1)
     return arg * arg - 2 * arg + 5
 
 
-epsilon = 0.005
-l = 0.02
+epsilon = 0.5
+l = 0.2
 a = -2
 b = 8
 
 while b - a > l:
+    k += 1
     y = (a + b - epsilon) / 2
     z = (a + b + epsilon) / 2
     if f(y) <= f(z):
@@ -17,3 +23,7 @@ while b - a > l:
 
 x = (a + b) / 2
 print(f"Минимум находится в точке {x} и равен {f(x)}")
+print(f"k = {k}")
+print(f"N = {N}")
+print(f"R(N) = {1 / (2 ** (N / 2))}")
+print(a, ' ', b)
--- 6 sem/Методы оптимизации/lab1/part2.py
+++ 6 sem/Методы оптимизации/lab1/part2.py
@@ -1,31 +1,43 @@
+def get_fib(n):
+    fib = [1, 1]
+    while fib[-1] < n:
+        fib.append(fib[-1] + fib[-2])
+    return fib
+
+def calculate_N(a, b, epsilon):
+    right_part = abs(b - a) / epsilon
+    N = 0
+    while get_fib(N)[-1]<right_part:
+        N+=1
+    return len(get_fib(N)) - 1
+
+a = -2
+b = 8
+epsilon = 0.2
+N = calculate_N(a, b, epsilon)
+print(N)
+
 def f(arg):
     return arg * arg - 2 * arg + 5
 
-def fibonacci_method(a, b, epsilon):
-    def get_fib(n):
-        fib = [1, 1]
-        while fib[-1] < n:
-            fib.append(fib[-1] + fib[-2])
-        return fib
 
+def fibonacci_method(a, b, epsilon):
     n = (b - a) / epsilon
     fib = get_fib(n)
-    k = len(fib) - 1
+    k = 0
 
-    for i in range(1, k):
-        y = a + (fib[k - i - 1] / fib[k - i + 1]) * (b - a)
-        z = a + (fib[k - i] / fib[k - i + 1]) * (b - a)
+    while k != N - 3:
+        y = a + (fib[N - 2] / fib[N]) * (b - a)
+        z = a + (fib[N-1] / fib[N]) * (b - a)
 
         if f(y) < f(z):
             b = z
         else:
             a = y
+        k+=1
     x = (a + b) / 2
     return x, f(x)
 
-a = -2
-b = 8
-epsilon = 0.2
 
 x_min, f_min = fibonacci_method(a, b, epsilon)
 print(f"Минимум находится в точке {x_min}, f(x) = {f_min}")
