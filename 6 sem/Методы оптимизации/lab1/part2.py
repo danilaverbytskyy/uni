b36b5e3a7c8bcf6b419d1cb7facb85a5b44b6add
Danila <dod.shreder@gmail.com>
2025-03-19 13:44:16 +0300

    МО 1.1 1.2

--- 6 sem/Методы оптимизации/lab1/part2.py
+++ 6 sem/Методы оптимизации/lab1/part2.py
@@ -4,18 +4,21 @@ def get_fib(n):
         fib.append(fib[-1] + fib[-2])
     return fib
 
+
 def calculate_N(a, b, epsilon):
     right_part = abs(b - a) / epsilon
     N = 0
-    while get_fib(N)[-1]<right_part:
-        N+=1
+    while get_fib(N)[-1] < right_part:
+        N += 1
     return len(get_fib(N)) - 1
+
 
 a = -2
 b = 8
 epsilon = 0.2
 N = calculate_N(a, b, epsilon)
-k=0
+k = 0
+
 
 def f(arg):
     return arg * arg - 2 * arg + 5
@@ -26,16 +29,25 @@ def fibonacci_method(a, b, epsilon):
     n = (b - a) / epsilon
     fib = get_fib(n)
 
-    while k != N - 3:
+    while True:
         y = a + (fib[N - 2] / fib[N]) * (b - a)
-        z = a + (fib[N-1] / fib[N]) * (b - a)
+        z = a + (fib[N - 1] / fib[N]) * (b - a)
 
         if f(y) < f(z):
             b = z
         else:
             a = y
-        k+=1
 
+        if k == N - 3:
+            break
+        k += 1
+
+    y = z
+    z = y + epsilon
+    if f(y) <= f(z):
+        b = z
+    else:
+        a = y
     print(f"a = {a}, b = {b}")
     x = (a + b) / 2
     return x
