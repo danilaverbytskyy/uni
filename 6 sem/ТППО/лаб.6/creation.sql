0f2155ed75e87d287fcd7648a75486159ac6dade
Danila <dod.shreder@gmail.com>
2025-05-03 16:29:34 +0300

    Обманул в прошлый раз. ТППО5 и недоделанное ТППО 6

--- 6 sem/ТППО/лаб.6/creation.sql
+++ 6 sem/ТППО/лаб.6/creation.sql
@@ -4,14 +4,12 @@ CREATE TABLE Products (
     quantity INTEGER NOT NULL CHECK (quantity >= 0)
 );
 
-
 CREATE TABLE Orders (
     order_id SERIAL PRIMARY KEY,
     total_price DECIMAL(10, 2) NOT NULL CHECK (total_price >= 0),
     date DATE NOT NULL,
     date_of_recording TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
 
 CREATE TABLE Orders_Products (
     id SERIAL PRIMARY KEY,
